22a87f2ce8416306cd24927337236b16aa48be92
luyan <beacher.lu@qq.com>
2017-04-26 00:40:10 +0800

    add

--- src/leetcode/SingleNumber/Solutions.java
+++ src/leetcode/SingleNumber/Solutions.java
@@ -4,20 +4,21 @@ package leetcode.SingleNumber;
  * Created by luyan on 17/4/14.
  */
 public class Solutions {
+
     public static void main(String[] args) {
-        System.out.println(singleNumber(new int [] { 1,1,2,2,3,4,4}));
+        System.out.println(singleNumber(new int[] { 1, 1, 2, 2, 3, 4, 4 }));
 
     }
+
     public static int singleNumber(int[] nums) {
 
-        if (nums == null || nums.length<=0 )
-        {
+        if (nums == null || nums.length <= 0) {
             return 0;
         }
         int res = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            res = res^nums[i];
+            res = res ^ nums[i];
         }
-        return  res;
+        return res;
     }
 }
